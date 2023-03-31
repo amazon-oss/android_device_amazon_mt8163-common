c908adcb3c1830a37b8d1e512b1e7204629c1b9b
SamarV-121 <samarvispute121@pm.me>
2023-03-31 21:12:39 +0100

    mt8163-common: Import and build AIDL lights service from RM6785
    Co-authored-by: Demon000 <demonsingur@gmail.com>
    Co-authored-by: vasishath <vasishath@gmail.com>
    Co-authored-by: daniml3 <daniel@danielml.dev>
    Change-Id: I596bb8a723842087e3db9013e35f1b8e1a09717f

--- mt8163.mk
+++ mt8163.mk
@@ -66,6 +66,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1-service
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.mt8163
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
