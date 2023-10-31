87fc1bd5b1005c88870b26699446010f61f1e7ab
R0rt1z2 <me@r0rt1z2.com>
2023-10-31 12:17:47 +0100

    mt8163-common: Initial skeleton tree
    Change-Id: I9453cd4d8c3ea7589b1412f2e1645b1f50a4f1a9

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/amazon/mt8163-common
+
+# Inherit the proprietary files
+include vendor/amazon/mt8163-common/BoardConfigVendor.mk
