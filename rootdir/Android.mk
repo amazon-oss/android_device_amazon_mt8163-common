b3cd24281796e08f83361577008ede1cac262517
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-10-31 14:47:57 +0100

    mt8163-common: Switch to 2SI
    Change-Id: I9e03ad0f584b1fbc022502190c85db8542535495

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,6 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := fstab.mt8163_ramdisk
+LOCAL_MODULE_STEM := fstab.mt8163
+LOCAL_SRC_FILES := etc/fstab.mt8163
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.mt8163.rc
 LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional
