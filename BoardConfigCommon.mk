654b07a37c5c13059653ecdc90d72a60eb67cda8
R0rt1z2 <me@r0rt1z2.com>
2023-10-31 12:24:49 +0100

    mt8163-common: Set boot image flags

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -21,5 +21,13 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
 
+# Boot
+BOARD_KERNEL_BASE := 0x40078000
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x03f88000 --second_offset 0x00e88000 --tags_offset 0x07f88000 --header_version 0x0
+
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+
 # Inherit the proprietary files
 include vendor/amazon/mt8163-common/BoardConfigVendor.mk
