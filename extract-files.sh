00c8e9de245eee978dbf3612cff306c9bedef7e7
R0rt1z2 <me@r0rt1z2.com>
2024-01-04 12:51:50 +0100

    mt8163-common: Inject `libsensor` for camera blobs
    * dlopen failed: cannot locate symbol "_ZNK7android16SensorEventQueue5getFdEv" referenced by "/system/lib64/libcam.utils.sensorlistener.so"...

--- extract-files.sh
+++ extract-files.sh
@@ -248,6 +248,7 @@ function blob_fixup() {
             ;;
         vendor/lib*/libcam.utils.sensorlistener.so)
             patchelf --add-needed "libpthread_shim.so" "${2}"
+            patchelf --add-needed "libsensor.so" "${2}"
             ;;
         vendor/lib*/libcam1_utils.so)
             patchelf --add-needed "libxlog.so" "${2}"
