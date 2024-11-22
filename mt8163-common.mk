f660b11cd76f399623a581dcf6c3bcd4d83313eb
R0rt1z2 <me@r0rt1z2.com>
2024-11-22 18:05:34 +0100

    mt8163-common: Inherit HWUI and Dalvik configs

--- mt8163-common.mk
+++ mt8163-common.mk
@@ -11,5 +11,11 @@ PRODUCT_PACKAGES += \
     init.mt8163.usb.rc \
     ueventd.mt8163.rc
 
+# Call dalvik heap config
+$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
+
+# Call hwui memory config
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/amazon/mt8163-common/mt8163-common-vendor.mk)
