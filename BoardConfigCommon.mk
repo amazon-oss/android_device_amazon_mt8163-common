46b88870397101b001fb5c6be9cd4448481ddac0
Ewout van der Linden <IonKiwi@users.noreply.github.com>
2024-12-23 22:26:40 +0100

    mt8163-common: Enable `TARGET_OMX_LEGACY_RESCALING`
    * Fix video autoscaling on old OMX decoders.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,6 +67,9 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
 
+# OMX
+TARGET_OMX_LEGACY_RESCALING := true
+
 # Partitions
 TARGET_COPY_OUT_SYSTEM := system
 
