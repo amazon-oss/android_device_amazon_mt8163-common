193021442b43d60cf71b02fad6d1143dc87251ba
R0rt1z2 <me@r0rt1z2.com>
2025-08-20 13:39:41 +0200

    mt8163-common: Initial skeleton tree

--- Android.mk
+++ Android.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter douglas giza, $(TARGET_DEVICE)),)
+ifneq ($(filter douglas giza rook, $(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
--- mt8163-common.mk
+++ mt8163-common.mk
@@ -1,12 +1,8 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/amazon/mt8163-common/mt8163-common-vendor.mk)
