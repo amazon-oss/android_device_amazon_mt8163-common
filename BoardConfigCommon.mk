e0b0ca60d79052c3a683bb1c6f2aa900b80e4cf8
bengris32 <bengris32@protonmail.ch>
2025-09-26 14:31:18 +0100

    mt8163-common: Create skeleton tree
    Change-Id: Id8d7baf7fa3e6dd7d251f3e0c1397562a9096a16
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,9 @@
+#
+# SPDX-FileCopyrightText: 2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/amazon/mt8163-common
+
+# Inherit the proprietary files
+include vendor/amazon/mt8163-common/BoardConfigVendor.mk
---
+++ README.md
@@ -0,0 +1,8 @@
+# Common device tree for Amazon devices based on the MT8163 platform
+
+```
+#
+# SPDX-FileCopyrightText: 2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+```
