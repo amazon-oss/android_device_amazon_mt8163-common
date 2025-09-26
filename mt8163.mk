187c9e6219cc700b89fe68b9e8a187d36cd98750
bengris32 <bengris32@protonmail.ch>
2025-09-26 14:57:03 +0100

    mt8163-common: Add basic recovery init script
    Change-Id: I57675a9f1e390316a008a3ede76b9b507db701f2
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt8163.mk
+++ mt8163.mk
@@ -6,6 +6,10 @@
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Recovery
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init/init.recovery.mt8163.rc:recovery/root/init.recovery.mt8163.rc
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
