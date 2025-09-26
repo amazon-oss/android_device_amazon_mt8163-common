408b0e6fe8dcf8b3be22ed74bb7b2e4b6ca5b441
bengris32 <bengris32@protonmail.ch>
2025-09-26 14:31:31 +0100

    mt8163-common: Set Architecture flags
    Change-Id: I73d8c11fee3c7f4f520d072706ac0c05c2888172
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -5,5 +5,14 @@
 
 COMMON_PATH := device/amazon/mt8163-common
 
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+
+TARGET_USES_64_BIT_BINDER := true
+
 # Inherit the proprietary files
 include vendor/amazon/mt8163-common/BoardConfigVendor.mk
