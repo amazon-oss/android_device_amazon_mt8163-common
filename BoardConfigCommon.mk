99f61d73caa3ffda2af951548c02d57919ea0571
bengris32 <bengris32@protonmail.ch>
2025-09-27 00:53:07 +0100

    mt8163-common: Set partitions file system types
    Change-Id: I8a72c69ac2f5c0e7b96d0264dfc945e324d4748f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -48,6 +48,13 @@ TARGET_KERNEL_SOURCE := kernel/amazon/mt8163-4.9
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(shell pwd)/prebuilts/linaro/linux-x86/aarch64/aarch64-linux-gnu/bin/aarch64-linux-gnu-
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
+# Partitions
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+ifeq ($(TARGET_HAS_VENDOR_PARTITION),true)
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+endif
+
 # Platform
 TARGET_BOARD_PLATFORM := mt8163
 
