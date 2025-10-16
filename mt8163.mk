8d166e96ef43246330c17b648e0946b8388ce06e
bengris32 <bengris32@protonmail.ch>
2025-10-16 20:54:19 +0100

    mt8163-common: Build LiveDisplay HAL
    Change-Id: Icc0cf432cacb26a0a06420050c62809d0c9e5146
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt8163.mk
+++ mt8163.mk
@@ -88,6 +88,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.mt8163
 
+# Livedisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-mt8163
+
 # Media
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media/,$(TARGET_COPY_OUT_VENDOR)/etc) \
