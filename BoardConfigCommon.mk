4ec1ec68b13cdcfe4ffe89f52cf56bf442b38472
R0rt1z2 <me@r0rt1z2.com>
2026-05-03 19:18:24 +0200

    mt8163-common: Set-up inline kernel building
    For whatever reason, the prebuilt GCC compiler shits
    itself while building our kernel. Amazon used Linaro
    GCC to build the stock kernel, so mimic that.
    
    Change-Id: Id4fa7735348dcc188f373e261262b731c1875f3d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -14,5 +14,25 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 TARGET_ARCH_VARIANT_CPU := cortex-a53
 
+# Boot
+BOARD_KERNEL_BASE := 0x40078000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x03f88000 --second_offset 0x00e88000 --tags_offset 0x07f88000
+
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,32N2
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+
+# Kernel
+TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_HEADER_ARCH := arm
+TARGET_KERNEL_CONFIG := mt8163_defconfig
+TARGET_KERNEL_SOURCE := kernel/amazon/mt8163
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(shell pwd)/prebuilts/gcc/linux-x86/arm/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+
+TARGET_LINUX_KERNEL_VERSION := 4.9
+
 # Inherit the proprietary files
 include vendor/amazon/mt8163-common/BoardConfigVendor.mk
