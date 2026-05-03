5464c50652d6755ca8df41dcbfd98036d84afaab
R0rt1z2 <me@r0rt1z2.com>
2026-05-03 19:15:23 +0200

    mt8163-common: Set Architecture flags
    Change-Id: I5bf3210700febb74f570d331b0f04d76224c0503

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -6,5 +6,13 @@
 
 COMMON_PATH := device/amazon/mt8163-common
 
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_ARCH_VARIANT_CPU := cortex-a53
+
 # Inherit the proprietary files
 include vendor/amazon/mt8163-common/BoardConfigVendor.mk
