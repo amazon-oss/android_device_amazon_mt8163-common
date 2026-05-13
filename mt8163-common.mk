48f0bc388195b9ce0acee75cc567812c071d471e
R0rt1z2 <me@r0rt1z2.com>
2026-05-13 09:58:17 +0200

    mt8163-common: Configure Media stack
    Change-Id: I1fced5da0f4c197c32b676483178373d4e5e9e5c

--- mt8163-common.mk
+++ mt8163-common.mk
@@ -11,6 +11,14 @@ TARGET_BOOTANIMATION_HALF_RES := true
 PRODUCT_PACKAGES += \
     libion
 
+# Media (OMX)
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media/,$(TARGET_COPY_OUT_SYSTEM)/etc)
+
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs_google_video_le.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
